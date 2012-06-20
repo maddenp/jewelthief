4288a898729b3979a2e6f3979c36a1fdc5889bfc
Paul Madden <maddenp_at_colorado_dot_edu>
2012-06-20 16:29:09 -0600

    Update README, license info

--- src/jewelthief.java
+++ src/jewelthief.java
@@ -1,3 +1,11 @@
+// jewelthief
+// Written by: Paul Madden (maddenp@colorado.edu) 2006
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty. You should have
+// received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+
 import java.awt.*;
 import java.io.*;
 import java.net.*;
